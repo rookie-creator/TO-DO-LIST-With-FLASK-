df617d5b01cbecb72fbc81218698ad5fc81aff73
sahil vaidya <sahilvaidya34@gmail.com>
2025-07-26 21:07:33 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import db
+from sqlalchemy import String
+
+class User(db.Model):
+    id =db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(150),unique=True,nullable=False)
+    password=db.Column(db.String(200),nullable=False)
 
 class Task(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    status = db.Column(db.String(20),default='pending')
+    status = db.Column(db.String(20), default='pending')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+
+    user = db.relationship('User', backref='tasks')
